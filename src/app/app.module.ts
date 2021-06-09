9c1f1e6348591b5c40c0e6870b05313826ab1c02
Maitre-Pangolin <martin.wasselet@gmail.com>
2021-06-09 17:15:24 -0400

    Add all components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoteComponent } from './components/note/note.component';
+import { NotesComponent } from './components/notes/notes.component';
+import { AddNoteComponent } from './components/add-note/add-note.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NoteComponent,
+    NotesComponent,
+    AddNoteComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
