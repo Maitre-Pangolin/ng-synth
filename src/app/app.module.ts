37f800fa35d5bc0ac0b30b943c570041b32a54a5
Maitre-Pangolin <martin.wasselet@gmail.com>
2021-06-14 13:43:09 -0400

    Remove old components import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,7 @@ import {FormsModule} from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NoteComponent } from './components/note/note.component';
-import { NotesComponent } from './components/notes/notes.component';
-import { AddNoteComponent } from './components/add-note/add-note.component';
 import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
 import { AboutComponent } from './components/about/about.component';
 import { KeyboardComponent } from './components/keyboard/keyboard.component';
 import { KeyComponent } from './components/key/key.component';
@@ -20,11 +16,7 @@ import { PresetsComponent } from './components/presets/presets.component';
 @NgModule({
   declarations: [
     AppComponent,
-    NoteComponent,
-    NotesComponent,
-    AddNoteComponent,
     HeaderComponent,
-    FooterComponent,
     AboutComponent,
     KeyboardComponent,
     KeyComponent,
